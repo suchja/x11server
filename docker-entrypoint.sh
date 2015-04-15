9c3ceab19af43e16696fe763b1a85e92201970b6
Jan Suchotzki <jan@suchotzki.de>
2015-04-15 18:25:03 +0200

    Fixes authorization problem with xvfb not accepting connections from client

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,16 +6,15 @@ if [ -z "$VNC_PASSWORD" ]; then
 	exit 1
 fi
 
-# first we need our security cookie
-touch ~/.Xauthority
-mcookie | sed -e 's/^/add :0 . /' | xauth -q
+# first we need our security cookie and add it to user's .Xauthority
+mcookie | sed -e 's/^/add :0 MIT-MAGIC-COOKIE-1 /' | xauth -q
 
 # now place the security cookie with FamilyWild on volume so client can use it
 # see http://stackoverflow.com/25280523 for details on the following command
-xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f /Xauthority/.xserver.xauth nmerge -
+xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f /Xauthority/xserver.xauth nmerge -
 
-# now boot X-Server and give it sometime to start up
-Xvfb :0 -screen 0 1024x768x24 2>&1 &
+# now boot X-Server, tell it to our cookie and give it sometime to start up
+Xvfb :0 -auth ~/.Xauthority -screen 0 1024x768x24 2>&1 & 
 sleep 2
 
 # finally we can run the VNC-Server based on our just started X-Server
